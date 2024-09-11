d284521e366853d55c7715f9ace39622f635c6a6
nehalskdev <programmingwithnehalsk1999@gmail.com>
2024-09-11 14:23:16 +0530

    initializing repo

---
+++ app.js
@@ -0,0 +1,17 @@
+"strict mode";
+
+const btn = document.querySelector(".center-button");
+const colorBox = document.getElementById("colorBox");
+
+const randomInt = (min, max) => Math.floor(Math.random() * (max - min) + 1);
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+
+const initialBox = colorBox.style.visibility = 'hidden';
+
+btn.addEventListener("click", function () {
+  const color = randomColor();
+  document.body.style.backgroundColor = color;
+  colorBox.style.visibility = "visible";
+  colorBox.value = color;
+});
