e6eede6e89f70dd3c17d7c61817d2c98c436efb6
nehalskdev <programmingwithnehalsk1999@gmail.com>
2024-09-13 17:05:10 +0530

    switch to arrow function and added round bracket

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const randomInt = (min, max) => Math.floor(Math.random() * (max - min) + 1);
 const randomColor = () =>
   `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
 
-const initialBox = colorBox.style.visibility = 'hidden';
+const initialBox = (colorBox.style.visibility = 'hidden');
 
-btn.addEventListener("click", function () {
+btn.addEventListener("click",  () => {
   const color = randomColor();
   document.body.style.backgroundColor = color;
   colorBox.style.visibility = "visible";
